3259606c25c2f4c2a96e509aff000eab90573be5
nicodamn <nikosfikos@icloud.com>
2025-05-27 22:17:17 +0200

    added display + infinite game loop

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pygame
+from constants import *
 if __name__ == "__main__":
     def main():
+        pygame.init()
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         print("Starting Asteroids!")
+        print(f'Screen width: {SCREEN_WIDTH}')
+        print(f'Screen height: {SCREEN_HEIGHT}')
+        while(True):
+           for event in pygame.event.get():
+               screen.fill((0,0,0))
+               pygame.display.flip()
+               if event.type == pygame.QUIT:
+                   return
     main()
